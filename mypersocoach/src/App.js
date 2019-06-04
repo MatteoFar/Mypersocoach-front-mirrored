5755d709e68f76fa3b28fb08247b52038a555e66
matteo farigu <matteo.farigu@gmail.com>
2019-06-04 16:54:58 +0200

    update heading branch

--- mypersocoach/src/App.js
+++ mypersocoach/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import Login_screen from './Screen/Login_screen'
+import Sign_screen from './Screen/Sign_screen'
 import './App.css'
 
 import Start from './Screen/Start'
--- mypersocoach/src/serviceWorker.js
+++ mypersocoach/src/serviceWorker.js
@@ -10,11 +10,7 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-<<<<<<< HEAD
 const isLocalhost = Boolean(
-=======
-const isLocalhost = Boolean( 
->>>>>>> dev
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
