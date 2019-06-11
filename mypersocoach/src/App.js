4441702dbac2cee7cf3d80d5780a028b96a11d11
kahija <ouarezki.kahina@gmail.com>
2019-06-11 10:11:19 +0200

    Source2

--- mypersocoach/src/App.js
+++ mypersocoach/src/App.js
@@ -3,10 +3,13 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import './App.css'
 
 import Start from './Screen/Start'
-//import Heading from './Component/Heading'
 
+
+//import Heading from './Component/Heading'
 //import Heading from './components/Heading'
-import Symptome3 from "./Pages/Symptome3";
+//import Symptome3 from "./Pages/Symptome3";
+//import Source2 from "./Pages/Source2"
+import Source2Environment from "./Pages/Source2Environment"
 
 class App extends Component {
   render() {
@@ -17,7 +20,10 @@ class App extends Component {
                 <Route exact path="/start" component={Start} />  
                 {/*<Route exact path="/login" component={Login_screen} />
                 <Route exact path="/sign_up" component={Sign_screen} /> */}
-                <Route exact path="/" component={Symptome3} />
+                {/*<Route exact path="/" component={Symptome3} /> */}
+                {/* <Route exact path="/" component={Source2} /> */}
+                <Route exact path="/" component={Source2Environment}/>
+
           </Switch>
         </BrowserRouter>
       </div>
