2d1857e36b9f0e91fd9cb0e394b6a0076eb3c69a
Discaz <olivier.discazeaux@gmail.com>
2019-06-19 11:21:15 +0200

    display of question added

--- mypersocoach/src/Component/DisplayModal.js
+++ mypersocoach/src/Component/DisplayModal.js
@@ -5,30 +5,55 @@ class DisplayModal extends React.Component {
 
 state = {
 
-    text_intro: []
-
+    text_prob: [],
+    question: []
 }
 
 getText = () => {
-    fetch('http://localhost:3003/main_theme')
+  fetch('http://localhost:3003/main_theme')
         .then(res => res.json())
         .then(data => {
-            this.setState({ text_intro : data })
+            this.setState({ text_prob : data })
         })
    
-    }
+}
+
+getQuestion = () => {
+  fetch('http://localhost:3003/text_static/105')
+  .then(res => res.json())
+  .then(data => {
+      this.setState({ question : data })
+  })
+
+}
+
 
 componentDidMount(){
-    this.getText()
-  }
+  this.getQuestion()
+  this.getText()
+}
+
+
   
+
+
   render() { 
-return (
+
+
+    return (
+    
+  
+    <div>
+     <p>{this.state.question.map(question => (<p>{question.all_text}</p>))}</p>
+
     <div className="modal_container">
     <div className="display-modal">
       <div className="button" >
-        {this.state.text_intro.map(content => (
+        {this.state.text_prob.map(content => (
+          
+          
           <div >
+           
             <img className="image_icon" alt="icon" src={content.picture_src} />
             <p className="text">{content.all_text}</p>
           </div>
@@ -36,9 +61,10 @@ return (
       </div>
       </div>
     </div>
-
+    </div>
 )
 
   }
 }
+
 export default DisplayModal;
