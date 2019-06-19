75af1cd3c25219de0c5eacd7bd4304cd9c5db551
kahija <ouarezki.kahina@gmail.com>
2019-06-19 09:29:11 +0200

    pagesSource2environment with component

--- mypersocoach/src/App.js
+++ mypersocoach/src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
-import SignScreen from './Screen/SignScreen';
-import LoginScreen from './Screen/LoginScreen';
-import AnswerScreen from './Screen/AnswerScreen';
+import React from "react";
+import { Route, BrowserRouter, Switch, NavLink } from "react-router-dom";
+import SignScreen from "./Screen/SignScreen";
+import LoginScreen from "./Screen/LoginScreen";
+import AnswerScreen from "./Screen/AnswerScreen";
 
-import Start from './Screen/Start'
-
-
+import Start from "./Screen/Start";
 
 import Symptome3 from "./Pages/Symptome3";
-import Source2 from "./Pages/Source2"
-import Source2environment from "./Pages/Source2environment"
+import Source2 from "./Pages/Source2";
+import Source2environment from "./Screen/Source2environment";
 
 class App extends React.Component {
   render() {
@@ -18,17 +16,18 @@ class App extends React.Component {
       <div>
         <BrowserRouter>
           <Switch>
-                <Route exact path="/" component={LoginScreen} />
-                <Route exact path="/start" component={Start} />  
-                <Route exact path="/sign_up" component={SignScreen} /> 
-                <Route exact path="/answer_screen" component={AnswerScreen} />
-                <Route exact path="/symptome3" component={Symptome3} />
-                <Route exact path="/Source2" component={Source2} /> 
-                <Route exact path="/Source2environment" component={Source2environment}/>
+            <Route exact path="/" component={LoginScreen} />
+            <Route exact path="/start" component={Start} />
+            <Route exact path="/sign_up" component={SignScreen} />
+            <Route exact path="/answer_screen" component={AnswerScreen} />
+            <Route exact path="/symptome3" component={Symptome3} />
+            <Route exact path="/Source2" component={Source2} />
+            <Route exact path="/Source2environment" component={Source2environment} />
           </Switch>
         </BrowserRouter>
       </div>
-  )}
+    );
+  }
 }
 
 export default App;
