9b9b0e5db9fd9db3bbee92a06a5bdb4fd7aae409
matteo farigu <matteo.farigu@gmail.com>
2019-06-24 17:20:43 +0200

    symptome5 and 2 added, must be finished

---
+++ mypersocoach/src/Component/ResponseSymptome2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ResponseSymptome2.css"
+
+const ResponseSymptome2 = ({ Answer }) => {
+  return (
+    <div className="logoTextEnvironment">
+      <p>ceci</p>
+    </div>
+  );
+};
+
+export default ResponseSymptome2;
