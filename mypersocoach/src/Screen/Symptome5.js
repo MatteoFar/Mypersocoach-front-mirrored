859d74008343950e96d8a2aa0d18e7ff74684f7b
kahija <ouarezki.kahina@gmail.com>
2019-06-25 14:10:19 +0200

    axios page symptome5

--- mypersocoach/src/Screen/Symptome5.js
+++ mypersocoach/src/Screen/Symptome5.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import "./Source2environment.css";
 
@@ -11,48 +12,38 @@ import Answer from "../Component/Answer"
 import NotReally from "../Component/NotReally";
 import { validate } from "@babel/types";
 import ForwardStep from "../Component/ForwardStep";
+import Axios from "axios";
 
-class Source2Environment extends React.Component {
+class Symptome5 extends React.Component {
   state = {
     icon: [],
-    problem_origin: [],
     text_static: []
   };
 
-  getHeader = () => {
-    fetch("http://localhost:3001/icon/27")
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ icon: data[0] });
-      });
+  getHeader = async() => {
+    const res = await axios.get("http://localhost:3001/icon/27")
+    this.setState({ icon: res.data[0] });
+    
   };
-  getProblem = () => {
-    fetch("http://localhost:3001/problem_origin/1")
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ problem_origin: data[0] });
-      });
-  };
-  getTextStatic = () => {
-    fetch("http://localhost:3001/text_static/110")
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ text_static: data[0] });
-      });
+  
+  getTextStatic = async () => {
+    const res = await axios.get("http://localhost:3001/text_static/110")
+    this.setState({ text_static: res.data[0] });
+    
   };
 
   componentDidMount() {
     this.getHeader();
-    this.getProblem();
     this.getTextStatic();
   }
 
   render() {
+    const{icon,text_static}= this.state
     return (
       <div className="containerSource2Environment">
-        <IconMain icon={this.state.icon.picture_src} />
+        <IconMain icon={icon.picture_src} />
         {/* <TitlePage problem_origin={this.state.problem_origin} /> */} 
-        <HeadingText text_static={this.state.text_static.all_text} /> 
+        <HeadingText text_static={text_static.all_text} /> 
         <Answer/>
         <ForwardStep/>
         <NotReally />
@@ -60,4 +51,4 @@ class Source2Environment extends React.Component {
     );
   }
 }
-export default Source2Environment;
+export default Symptome5;
