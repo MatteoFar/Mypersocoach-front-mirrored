9e5518957fa22fbea649173b5a5b2e2978a2fd0a
Discaz <olivier.discazeaux@gmail.com>
2019-06-25 12:20:43 +0200

    modal works properly

--- mypersocoach/src/Component/Modal.js
+++ mypersocoach/src/Component/Modal.js
@@ -1,71 +1,73 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import './Problem.css';
 import ModalGoal from "./ModalGoal";
+
+
+
 
 const nl2br = require('react-nl2br');
 
 
-class Modal extends React.Component {
+const Modal = ({id, text1, text2, icon})  =>{
 
-    state = { 
-      currentItemId:  undefined
+  const node = useRef();
+
+  const [open, setOpen] = useState(false);
+
+ 
+  
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click
+    setOpen(false);
+  };
+  
+
+  const handleChange = () => {
+    setOpen(false);
+  };
+
+
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick);
+  // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
     };
+  }, []);
 
-    showModal = (id) => {
-      const currentItemId = id !== this.state.currentItemId ? id : undefined
-      this.setState({ currentItemId })
-    };
-    
 
-  hideModal = () => {
-      this.setState({ show: false });
-    };
-
-render(){
-
-console.log('modalplouc',this.props.id,this.props.text2,this.props.icon)
+ 
 
 return(
 
  <div>
      
-        <div >
-        <img className="image_icon"  onClick={() => this.showModal(this.props.id)} alt="icon" src={this.props.icon} />
-        {
-          this.state.currentItemId === this.props.id ? (
-  <ModalGoal text2={nl2br(this.props.text2)} src={this.props.icon} show={this.state.currentItemId} handleClose={this.hideModal} />
-  ) : null}
-
-
-        <p className="text">{this.props.text1}</p>
-       
+     <div ref={node}  onClick={e => setOpen(!open)}>
         
+        
+        
+        <div >
+        
+        <div>
+        <img className="image_icon" onClick={e => handleChange(id, text1, text2, icon)} alt="icon" src={icon} />
+       
         </div>
+        {open && (<ModalGoal text2={nl2br(text2)} src={icon} show={setOpen} />)}
  
+      <p className="text">{text1}</p>
+       </div>
+      
+      </div>      
  
 </div>
 
 )
 }
-}
 
-// const ModalGoal  = ({src, show, handleClose, text2}) => {
-//   const showHideClassname = show ? "modal-display-block" : "modal-display-none";
-   
-//  return(
-//      <div className={showHideClassname} >
-         
-//      <div  className="modal_Cards" >
-//      <div className="Problem_modal">
-//      <img className="icon_problem"  alt="icon_problem" src={src}/>
-//      <p>{text2}</p>
-//      <button type="button" name ="C'est parti !">C'est parti !</button>
-    
-//      </div>
-//      </div>
-//      </div>
-     
-//      )
- 
-//  }
+
 export default Modal;
