7e70561e7b12ee5ed88886ec04f7241e92156030
said <freemankaysersoze2015@gmail.com>
2019-06-26 17:08:23 +0100

    commit axios answer form

--- mypersocoach/src/App.test.js
+++ mypersocoach/src/App.test.js
@@ -5,13 +5,7 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-<<<<<<< HEAD
+
   ReactDOM.unmountComponentAtNode(div); 
-=======
-<<<<<<< HEAD
-  ReactDOM.unmountComponentAtNode(div);
-=======
-  ReactDOM.unmountComponentAtNode(div); 
->>>>>>> dev
->>>>>>> dev
+
 });
