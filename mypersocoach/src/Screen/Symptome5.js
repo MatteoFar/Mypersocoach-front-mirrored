3eb023c9552e82a970a5487d2d1e982c7c78992b
David Mermet <davidmer12@gmail.com>
2019-06-28 09:54:49 +0200

    Code update and creation of the symptoms pages

--- mypersocoach/src/Screen/Symptome5.js
+++ mypersocoach/src/Screen/Symptome5.js
@@ -11,13 +11,15 @@ import NextStep from "../Component/NextStep";
 import Answer from "../Component/Answer"
 import NotReally from "../Component/NotReally";
 import { validate } from "@babel/types";
-import ForwardStep from "../Component/ForwardStep";
+import Validate from "../Component/Validate";
 import Axios from "axios";
 
 class Symptome5 extends React.Component {
   state = {
     icon: [],
-    text_static: []
+    text_static: [],
+    text_static2: [],
+    text_static3: []
   };
 
   getHeader = async() => {
@@ -32,20 +34,34 @@ class Symptome5 extends React.Component {
     
   };
 
+  getTextStatic2 = async() => {
+    const res = await axios.get("http://localhost:3001/text_static/140")
+    this.setState({text_static2: res.data[0]});
+  };
+
+  getTextStatic3 = async() => {
+    const res = await axios.get("http://localhost:3001/text_static/116")
+    this.setState({text_static3: res.data[0]});
+  };
+
+
+
   componentDidMount() {
     this.getHeader();
     this.getTextStatic();
+    this.getTextStatic2();
+    this.getTextStatic3();
   }
 
   render() {
-    const{icon,text_static}= this.state
+    const{icon,text_static,text_static2,text_static3}= this.state
     return (
       <div className="containerSource2Environment">
         <IconMain icon={icon.picture_src} />
         {/* <TitlePage problem_origin={this.state.problem_origin} /> */} 
         <HeadingText text_static={text_static.all_text} /> 
-        <Answer/>
-        <ForwardStep/>
+        <Answer text_static2={text_static2}/>
+        <Validate text_static3={text_static3.all_text}/>
         <NotReally />
       </div>
     );
