3290f077fa9dd9117a254498d0b753757ed4a313
David Mermet <davidmer12@gmail.com>
2019-06-28 12:49:51 +0200

    Update of the buttons (css and dynamic names) of the file 'Source2environment.js'

--- mypersocoach/src/Component/ForwardStep.js
+++ mypersocoach/src/Component/ForwardStep.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./ForwardStep.css"
 
-const ForwardStep = ({ text_static }) => {
+const ForwardStep = ({ text_static2 }) => {
     return (
       <div className="forward_step_container">
-        <button href="#" className="buttonForward_Step" type="button" name="">{text_static}</button>
+        <button href="#" className="buttonForward_Step" type="button" name="">{text_static2}</button>
       </div>
     );
   }
