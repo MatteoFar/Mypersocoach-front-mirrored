ab96c723b05f1032e7a64e0b6d012b96cab7e5af
Kamel Belkheiri <kamel.belkheiri75@gmail.com>
2019-07-05 10:02:50 +0200

    routes origin problem

--- mypersocoach/src/Component/Origins.js
+++ mypersocoach/src/Component/Origins.js
@@ -13,13 +13,41 @@ class Origins extends React.Component {
   
   handleClick = () => {
    
-    this.props.history.push({
-      pathname: "/Source2environment", 
-      state: { summaryId: this.props.summaryId }
-      });
+    if(this.props.problem_origins.id === 1) {
+      this.props.history.push({
+        pathname: "/Source2environment", 
+        state: { summaryId: this.props.summaryId }
+        });
+    }
+
+    if(this.props.problem_origins.id === 2) {
+      this.props.history.push({
+        pathname: "/Source2comportement", 
+        state: { summaryId: this.props.summaryId }
+        });
+    }
+
+    if(this.props.problem_origins.id === 3) {
+      this.props.history.push({
+        pathname: "/Source2capacites", 
+        state: { summaryId: this.props.summaryId }
+        });
+    }
+
+    if(this.props.problem_origins.id === 4) {
+      this.props.history.push({
+        pathname: "/Source2croyances", 
+        state: { summaryId: this.props.summaryId }
+        });
+    }
+    
   }
   
   render() {
+    console.log('test de map origin',this.props.problem_origins);
+    console.log('id de summary', this.props.summaryId);
+    
+    
   return (
       <>
      {/* <div className="logoTextSymptome3">
