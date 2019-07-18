4ff79ed360ce4baf5cd3afbbaab67c92c80184e3
kahija <ouarezki.kahina@gmail.com>
2019-07-18 12:57:23 +0200

    modif modal profil

--- mypersocoach/src/Component/ModalProfil.js
+++ mypersocoach/src/Component/ModalProfil.js
@@ -32,9 +32,14 @@ class ModalProfil extends React.Component {
             <img className="belief_image" src={this.props.icon_modal_profil3} alt={this.props.alt}
             />
             <p>{nl2br(this.props.text_modal_profil)}{nl2br(this.props.text_modal_profil2)}{nl2br(this.props.text_modal_profil3)}</p>
+            <p>{this.props.response} </p>
             <p>{this.props.respAction}</p>
             <p>{this.props.date_modal_profil3}</p>
-            <p>{this.props.date}{this.props.responseEnvironnement}{this.props.responseCroyance}{this.props.responseCapacite}{this.props.responseComportement}{this.props.response} </p>
+            <p>{this.props.date}</p>
+            <p>{this.props.responseEnvironnement}</p>
+            <p>{this.props.responseCroyance}</p>
+            <p>{this.props.responseCapacite}</p>
+            <p>{this.props.responseComportement}</p>
             <p onClick={this.props.CloseModal} className="pass_continue">
               {this.props.modal_finish}
             </p>
