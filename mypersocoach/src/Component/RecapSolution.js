4df54746dfb46baf117d4335d53e5ddb0ccea2e6
Kamel Belkheiri <kamel.belkheiri75@gmail.com>
2019-07-18 15:38:15 +0200

    fix button remove action

--- mypersocoach/src/Component/RecapSolution.js
+++ mypersocoach/src/Component/RecapSolution.js
@@ -10,26 +10,43 @@ class RecapSolution extends React.Component {
         response :'',
         isReply: false,
         summaryId: '',
+        iconValidate :"",
+        iconValidate2 :'',
+        selectedButton: null
         
     }
 
- 
+    getIconValidate = async () => {
+        const res = await axios.get('http://localhost:3001/icon/39')
+        this.setState({ iconValidate : res.data[0] })
+    }
 
 
+    getIconValidate2 = async () => {
+        const res = await axios.get('http://localhost:3001/icon/40')
+        this.setState({ iconValidate2 : res.data[0] })
+    }
 
-    handleClick = async (action) =>{
-       
-       
-   
+    handleClick = async (action, index) =>{
+    
+    
+            console.log('mon index :', index, action);
+            console.log('taille tab : ',this.state.addActions.indexOf(action));
+
+        this.setState({ selectedButton : index })
+        
+
         this.setState({response: action}); 
-      console.log('est pas là ma state?',this.state.response);
+        console.log('est pas là ma state?',this.state.response);
         await this.setState({response: action});
-    console.log('si elle est là',this.state.response);
+        console.log('si elle est là',this.state.response);
 
         this.setState({
             summaryId: this.props.summaryId, 
             isReply: !this.state.isReply
         })
+
+       
         
         console.log('is reply', this.state.isReply)
         
@@ -58,11 +75,14 @@ class RecapSolution extends React.Component {
                     }
             });
           }
-    ))
-
-        
+        ))   
     }
-        
+       
+    
+    componentDidMount () {
+        this.getIconValidate()
+        this.getIconValidate2()
+    }
        
     
 
@@ -71,22 +91,34 @@ class RecapSolution extends React.Component {
 
     render(){
         // console.log('etat de la state backsubmit:', this.props.backSubmit);
+        console.log('ma state :', this.state.iconValidate);
+        
+        const {iconValidate, iconValidate2} = this.state
+        
+        
         
         return (
             <div>
 
-                {this.state.addActions.map(addAction => {
+                {this.state.addActions.map((addAction , index )=> {
                     
                         if(addAction.length >= 1 ){
                             return(
                                 
                                 <div>
-                                    <p>{addAction}<button onClick={()=> this.handleClick(addAction)}>Valider</button></p>
-                                </div>
-                            )
-                        
-                        }
 
+                                    <p>
+                                    {addAction}
+                                 
+                                        <img src = {this.state.selectedButton === index ? iconValidate2.picture_src : iconValidate.picture_src}
+                                        alt ="" onClick={()=> this.handleClick(addAction, index)} />
+                                    
+                                    </p>
+
+                                </div>
+                            ) 
+                        }
+                      
                         else{
                             
                             return null
