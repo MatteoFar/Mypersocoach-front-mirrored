61aa9af460ca93d0b57c7e459aadd95e76c880a5
Discaz <olivier.discazeaux@gmail.com>
2019-07-19 15:22:10 +0200

    resolution of several warnings

--- mypersocoach/src/Component/RecapSolution.js
+++ mypersocoach/src/Component/RecapSolution.js
@@ -110,14 +110,14 @@ class RecapSolution extends React.Component {
                         if(addAction.length >= 1 ){
                             return(
                                 
-                                <div className="bla">
+                                <div key={index} className="formActionBlock">
 
-                                    <p className="blo">
+                                    <p className="formActionInput ">
                                     {addAction}
                                  
                                     </p>
 
-                                    <img className="bli" src = {this.state.selectedButton === index ? iconValidate2.picture_src : iconValidate.picture_src}
+                                    <img className="formActionIcon" src = {this.state.selectedButton === index ? iconValidate2.picture_src : iconValidate.picture_src}
                                         alt ="" onClick={()=> this.handleClick(addAction, index)} />
                                     
 
