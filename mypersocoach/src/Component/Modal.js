5b0a7027bb5038e9794376ac94be1c21e1d5d677
MatteoFar <matteo.farigu@gmail.com>
2019-07-22 21:06:29 +0200

    fix? the modal

--- mypersocoach/src/Component/Modal.js
+++ mypersocoach/src/Component/Modal.js
@@ -1,21 +1,14 @@
 import React, { useEffect, useState, useRef } from "react";
-import './Problem.css';
+import "./Problem.css";
 import ModalGoal from "./ModalGoal";
 
+const nl2br = require("react-nl2br");
 
-
-
-const nl2br = require('react-nl2br');
-
-
-const Modal = ({id, text1, text2, icon, alt})  =>{
-
+const Modal = ({ id, text1, text2, icon, alt }) => {
   const node = useRef();
 
   const [open, setOpen] = useState(false);
 
- 
-  
   const handleClick = e => {
     if (node.current.contains(e.target)) {
       // inside click
@@ -24,47 +17,59 @@ const Modal = ({id, text1, text2, icon, alt})  =>{
     // outside click
     setOpen(false);
   };
-  
 
   const handleChange = () => {
     setOpen(false);
   };
 
-  
-
   useEffect(() => {
     // add when mounted
     document.addEventListener("mousedown", handleClick);
-  // return function to be called when unmounted
+    // return function to be called when unmounted
     return () => {
       document.removeEventListener("mousedown", handleClick);
     };
   }, []);
 
-
-return(
-
- <div className="displayModalCentered">
-     
-     <div className="image_icon" ref={node}  onClick={e => setOpen(!open)} key= "">
-        
-        <div >
-        
-        
-        <img  onClick={e => handleChange(id, text1, text2, icon)}  src={icon} alt={alt}/>
-       
-        
-       
-        {open && (<ModalGoal id={id} text2={nl2br(text2)} src={icon} alt={alt} show={setOpen} />)}
-     
+  return (
+    <>
+    
+    <div className="displayModalCentered">
+      <div
+        className="image_icon"
+        ref={node}
+        onClick={e => setOpen(!open)}
+        key=""
+      >
+        <div>
+          <img
+            onClick={e => handleChange(id, text1, text2, icon)}
+            src={icon}
+            alt={alt}
+          />
         </div>
+      </div>
+
+      
+
+        <p>{text1}</p>
+        <div className="modalTest" ref={node} onClick={e => setOpen(!open)}>
+          {open && (
+            <ModalGoal
+              id={id}
+              text2={nl2br(text2)}
+              src={icon}
+              alt={alt}
+              show={setOpen}
+            />
+          )}
+        </div>
+    </div>
+
+    
        
-      </div>      
- <div ><p >{text1}</p></div>
-</div>
-
-)
-}
-
+        </>
+  );
+};
 
 export default Modal;
